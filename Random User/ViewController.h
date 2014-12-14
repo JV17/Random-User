7770e152eae7821a1bfa13a8e392e1a6be6e8cb9
Jorge Valbuena <jorgevalbuena2@gmail.com>
2014-12-14 13:19:15 -0500

    Initial Commit

---
+++ Random User/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Random User
+//
+//  Created by Jorge Valbuena on 2014-12-14.
+//  Copyright (c) 2014 com.jorgedeveloper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Random User/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Random User
+//
+//  Created by Jorge Valbuena on 2014-12-14.
+//  Copyright (c) 2014 com.jorgedeveloper. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
